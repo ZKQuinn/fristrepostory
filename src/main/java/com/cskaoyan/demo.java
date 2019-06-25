f5ec2f36fa0c2e97946516df6968dcd5a5e1661e
ZKQuinn <991631075@qq.com>
2019-06-25 23:30:28 +0800

    cvbgsrf

---
+++ src/main/java/com/cskaoyan/demo.java
@@ -0,0 +1,12 @@
+package com.cskaoyan;
+
+/**
+ * @Author: kwin
+ * @Date: Create in 23:28 2019/6/25
+ */
+public class demo {
+
+    public static void main(String[] args) {
+        System.out.println("sahdgs");
+    }
+}
